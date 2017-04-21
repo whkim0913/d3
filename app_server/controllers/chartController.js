91a9a739efa0861b4719eba4115a43519a9478f7
Kim, Woohyun <whkim@aim.co.kr>
2017-04-21 14:48:00 +0900

    test

--- app_server/controllers/chartController.js
+++ app_server/controllers/chartController.js
@@ -5,34 +5,7 @@ var db = require('../../config/db');
 var express = require('express');
 
 module.exports.chart_get = function(req, res){
-    var sql = 'SELECT B.SEQ, A.VALUE AS Y  ,SUBSTR(A.TIMEKEY, 0, 14) AS X ' +
-        'FROM DCDCDATA_PHOTO A, DCDCPHOTOKEY B, DCEQUIPMENT C ' +
-        'WHERE A.SEQ = B.SEQ AND B.EQUIPMENTID = C.EQUIPMENTID AND ITEMID = :ITEMID ' +
-        'ORDER BY A.SEQ';
-    var itemId = req.body.itemId;
-    var params = new Array();
-    params.push(itemId);
-
-    db.doConnect(function(err, connection){
-
-        if (err) {
-            console.log("ERROR: Unable to get a connection ");
-            return callback(err);
-        } else {
-            db.doExecute(
-                connection, sql ,params// PASS BIND PARAMS IN HERE - SEE ORACLEDB DOCS
-                , function(err, result) {
-                    if (err) {
-                        db.doRelease(connection);     // RELEASE CONNECTION
-                        return callback(err);                 // ERROR
-                    } else {
-                        db.doRelease(connection);     // RELEASE CONNECTION
-                        res.render('chart', { result: result.rows });
-                    }
-                }
-            );
-        }
-    });
+    res.render('chart');
 };
 
 module.exports.chart_create_post = function(req, res, next){
